39a203c4cdb40ad94820ae870c08449a2c730b70
Phuthuma <phuthumaloyisopetse@gmail.com>
2016-09-19 15:40:55 +0200

    MyFirstProject

---
+++ example/example/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim surname As String
+
+        System.Console.WriteLine("Enter Name:")
+        name = System.Console.ReadLine()
+        System.Console.WriteLine("Enter Surname:")
+        surname = System.Console.ReadLine()
+
+        System.Console.WriteLine("My name is " & name & surname)
+        System.Console.ReadLine()
+
+
+    End Sub
+
+End Module
---
+++ example/example/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("example")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("example")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9af5c5d7-2af6-4f41-a612-31d311a1e64d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
